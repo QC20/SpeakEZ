1115e35995753ab691ec04167d8ab6b76830eeef
QC20 <xgv866@alumni.ku.dk>
2024-01-15 22:45:22 +0100

    refactor

---
+++ check.py
@@ -0,0 +1,3 @@
+import pyttsx3
+
+dir(pyttsx3)
--- check_voices.py
+++ check_voices.py
@@ -1,5 +1,5 @@
 import pyttsx3
-engine = pyttsx3.init('sapi5')
+engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 for voice in voices:
     print(voice, voice.id)
